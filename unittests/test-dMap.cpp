d693cbc94919f42b0020c45ab27626a0b7365a87
Julia <julia.999@gmx.de>
2018-12-05 07:02:00 -0800

    dmap test lauft jetzt, noch fehlerhaft

--- unittests/test-dMap.cpp
+++ unittests/test-dMap.cpp
@@ -1,36 +1,37 @@
+
 #include "catch.hpp"
 
 
 #include "helper.hpp"
 #include "myfs.h"
+
+
 #include "dMap.h"
-#include "blockdevice.h"
+
 
 TEST_CASE( "Set/unset/find Blocks", "[dMap]" ) {
 
-	dMap dmap = new dMap();
+
+	dMap* dmap = new dMap();
 
 
 	 SECTION("get 2 Blocks") {
-			 dmap.setUsed(1);
+			 dmap->setUsed(1);
 
 			 int * result  = new int [3];
-			 dmap.getFreeBlocks(4,&result);
+			 dmap->getFreeBlocks(4,&result);
 			 REQUIRE( result[0]== 0 );
 			 REQUIRE( result[1]== 2 );
 			 REQUIRE( result[2]== 3 );
 
 			 delete[] result;
-		 }
-
-
-
+	 }
 
 	 SECTION("get 3 Blocks with 1 block al") {
-		 dmap.setUsed(1);
+		 dmap->setUsed(1);
 
 		 int * result  = new int [3];
-		 dmap.getFreeBlocks(4,&result);
+		 dmap->getFreeBlocks(4,&result);
 		 REQUIRE( result[0]== 0 );
 		 REQUIRE( result[1]== 2 );
 		 REQUIRE( result[2]== 3 );
@@ -38,11 +39,7 @@ TEST_CASE( "Set/unset/find Blocks", "[dMap]" ) {
 		 delete[] result;
 	 }
 
-
-
-
-
-
+	 delete dmap;
 }
 
 
