74a13662daacb2afc5a98e4d477c41119b906d00
perkoneka <perkone.ka@gmail.com>
2018-12-11 14:22:29 +0200

    test fat

---
+++ unittests/test-fat.cpp
@@ -0,0 +1,34 @@
+#include "catch.hpp"
+#include "helper.hpp"
+#include "fat.h"
+#include "blockdevice.h"
+
+TEST_CASE( "Set/get/delete Allocated Blocks", "[fat]" ) {
+
+	MyFAT* fat = new MyFAT();
+
+	 SECTION("delete") {
+		 int table  = new int [];
+		 fat->unLink(5);
+		 //REQUIRE(table[5] == -1);
+		 delete[] table;
+	 }
+	 SECTION("link") {
+		 int table[];
+		 int * next;
+		 next = new int [5]; //int * next  = new int [5];
+		 *next=4;
+		 fat->link(5, next);
+		 REQUIRE(table[5] == 4);
+		 delete[] table;
+		 }
+	 SECTION("get") {
+		 	 int current = 4;
+		 	 int *next =current;
+			 int table[];
+			 fat->getNext(5, next);
+			 REQUIRE(table[5] == 4);
+			 delete[] table;
+			 }
+	 delete fat;
+}
