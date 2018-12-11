53fa4e60a17134f310eb8a59b8f262cfdffecade
cebo1012 <celine.borchers@online.de>
2018-12-11 16:44:53 +0100

    Unittest fuer MyFile.cpp

---
+++ unittests/test-myfile.cpp
@@ -0,0 +1,29 @@
+
+#include "catch.hpp"
+
+
+#include "helper.hpp"
+#include "myfs.h"
+
+
+#include "MyFile.h"
+
+
+TEST_CASE( "Compare File// Getter- and Settermethods", "[MyFile]" ) {
+
+
+	MyFile* myfile = new MyFile("myfile.txt",getuid(),getgid(),NULL,NULL,time(NULL),time(NULL),time(NULL),-1);
+
+
+	 SECTION("Compare File") {
+		 MyFile* similarFile = new MyFile("myfile.txt",getuid(),getgid(),NULL,NULL,time(NULL),time(NULL),time(NULL),-1);
+         bool vergleich =(myfile == similarFile);
+		 
+         REQUIRE( vergleich == true);
+		 
+         delete similarFile;
+         delete vergleich;
+	 }
+
+	 delete myfile;
+}
