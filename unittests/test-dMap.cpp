976bb037fd32a5e1410dddd206a61eadca942e13
Julia <julia.999@gmx.de>
2018-12-11 09:33:00 -0800

    dmap tests hinzugefügt

--- unittests/test-dMap.cpp
+++ unittests/test-dMap.cpp
@@ -14,7 +14,6 @@ TEST_CASE( "Set/unset/find Blocks", "[dMap]" ) {
 
 	dMap* dmap = new dMap();
 
-
 	 SECTION("get 3 Blocks") {
 		 	dmap->setUsed(1);
 
@@ -27,6 +26,54 @@ TEST_CASE( "Set/unset/find Blocks", "[dMap]" ) {
 		 delete[] result;
 
 	 }
+
+	 SECTION("get 3 Blocks after block is set free") {
+
+		 	dmap->setUsed(1);
+		 	dmap->setUnused(1);
+
+		 int * result  = new int [3];
+
+		 dmap->getFreeBlocks(3,&result);
+		 REQUIRE( result[0] == 0);
+		 REQUIRE( result[1] == 1);
+		 REQUIRE( result[2] == 2);
+		 delete[] result;
+
+	 }
+
+
+	 SECTION("get blocks with nearly full dmap") {
+
+		 for(int i= 0; i<BLOCK_NUMBER-1; i++){
+			 dmap->setUsed(i);
+		 }
+
+		 int * result  = new int [1];
+
+		 REQUIRE(dmap->getFreeBlocks(1,&result)==0);
+		 REQUIRE(result[0]==BLOCK_NUMBER-1);
+
+		 delete[] result;
+
+	 }
+
+	 SECTION("get blocks with full dmap") {
+
+		 for(int i= 0; i<BLOCK_NUMBER; i++){
+			 dmap->setUsed(i);
+		 }
+
+		 int * result  = new int [1];
+		 REQUIRE(dmap->getFreeBlocks(1,&result)==-1);
+		 delete[] result;
+
+	 }
+
+
+
+
+
 
 
 	 delete dmap;
