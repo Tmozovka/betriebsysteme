efa4370bff23c6bf56acbee3517ebe9f48426424
Tmozovka <tmozovka@gmail.com>
2018-12-14 15:57:34 +0100

    add defines

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -14,6 +14,17 @@
 #define NUM_DIR_ENTRIES 64
 #define NUM_OPEN_FILES 64
 
+#define SP_START 0
+#define SP_SIZE 1
+#define DMAP_START 1
+#define DMAP_SIZE 118
+#define FAT_START 119
+#define FAT_SIZE 118
+#define ROOT_START 237
+#define ROOT_SIZE 64
+#define BLOCKS_START 301
+
+
 // TODO: Add structures of your file system here
 
 
